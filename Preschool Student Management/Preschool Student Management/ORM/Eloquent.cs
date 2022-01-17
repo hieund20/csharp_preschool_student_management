c9ed6f4e3e166b02119da507fb5783bbe24aa75d
dinhdjj <dinhdjj@gmail.com>
2022-01-17 21:00:09 +0700

    chore: create and update for model

--- Preschool Student Management/Preschool Student Management/ORM/Eloquent.cs
+++ Preschool Student Management/Preschool Student Management/ORM/Eloquent.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
 using System.Linq;
+using Preschool_Student_Management;
 
 namespace Preschool_Student_Management.ORM
 {
@@ -17,6 +18,8 @@ namespace Preschool_Student_Management.ORM
 		/// Contain all attibutes of the model
 		/// </summary>
 		protected Dictionary<string, string> attributes  = new Dictionary<string, string>();
+
+		protected List<string> changedAttributeNames = new List<string>(); 
 
 		/// <summary>
 		/// Determine whether the model is exists in database
@@ -36,6 +39,7 @@ namespace Preschool_Student_Management.ORM
 		public void SetAttribute(string field, string value)
 		{
 			this.attributes[field] = value;
+			this.changedAttributeNames.Add(field);
 		}
 
 		/// <summary>
@@ -172,9 +176,10 @@ namespace Preschool_Student_Management.ORM
 				while (reader.Read())
 				{
 					var model = new T();
+					model.exists = true;
 					for (int lp = 0; lp < reader.FieldCount; lp++)
 					{
-						model.SetAttribute(reader.GetName(lp), reader.GetValue(lp).ToString());
+						model.attributes[reader.GetName(lp)] = reader.GetValue(lp).ToString();
 					}
 					models.Add(model);
 				}
@@ -216,6 +221,77 @@ namespace Preschool_Student_Management.ORM
 		/// </summary>
 		public T Find(string value) {
 			return this.Where(this.KeyName, "=", value).First();
+		}
+
+		/// <summary>
+		/// Save or create a model to database
+		/// </summary>
+		public void Save() {
+			if (this.exists)
+			{
+				this.SaveAsUpate();
+			}
+			else 
+			{
+				this.SaveAsInsert();
+			}
+
+			this.exists = true;
+		}
+
+		/// <summary>
+		/// Save but insert data to database
+		/// </summary>
+		protected void SaveAsInsert() {
+			var columns = "(";
+			var values = "(";
+
+			var isFirst = true;
+			foreach (var attribute in this.attributes)
+			{
+				if (isFirst)
+				{
+					columns += "`" + attribute.Key + "`";
+					values += "\"" + attribute.Value + "\"";
+					isFirst = false;
+				}
+				else
+				{
+					columns += ", `" + attribute.Key + "`";
+					values += ", \"" + attribute.Value + "\"";
+				}
+			}
+			columns += ")";
+			values += ")";
+			Utils.insertQuery("INSERT INTO `" + this.TableName +"` " + columns + " VALUES " + values);
+		}
+
+		/// <summary>
+		/// Save but update data in database
+		/// </summary>
+		protected void SaveAsUpate()
+		{
+			if (this.changedAttributeNames.Count == 0) {
+				return;
+			}
+
+			var query = "UPDATE `" + this.TableName +"` SET";
+
+			var isFirst = true;
+			foreach (var name in this.changedAttributeNames)
+			{
+				if (isFirst)
+				{
+					query += " `" + name + "`=\"" + this.attributes[name] + "\"";
+					isFirst = false;
+				}
+				else
+				{
+					query += ", `" + name + "`=\"" + this.attributes[name] + "\"";
+				}
+			}
+			query += " WHERE `" + this.KeyName + "` = \"" + this.GetAttribute(this.KeyName) + "\"";
+			Utils.insertQuery(query);
 		}
 	}
 }
